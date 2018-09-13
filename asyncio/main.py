453fb39b57b441f5b1be8e2d1962840cff417a53
Boguslav <Boguslav_Pozniak@epam.com>
2018-09-13 11:24:28 +0300

    Finding sum of prime numbers from random sequence using asyncio

---
+++ asyncio/main.py
@@ -0,0 +1,28 @@
+import asyncio
+from random import randint
+
+
+async def is_prime(x):
+    if x > 1:
+        for i in range(2, x):
+            if (x % i) == 0:
+                break
+        else:
+            print(str(x) + ' is prime')
+            return x
+    return 0
+
+
+async def prime_sum(first, last):
+    res = 0
+    for i in range(first, last+1):
+        res += await is_prime(i)
+    print(res)
+
+
+if __name__ == '__main__':
+    start = randint(1, 50)
+    finish = randint(1, 50) + start + 1
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(prime_sum(start, finish))
+    loop.close()
