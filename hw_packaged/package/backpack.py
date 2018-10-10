6cb0f0a87e0b97d519b687aa6eda3a88899c833f
Boguslav <Boguslav_Pozniak@epam.com>
2018-10-10 09:55:14 +0300

    Packaged version of knapsack solving screept

---
+++ hw_packaged/package/backpack.py
@@ -0,0 +1,84 @@
+from collections import namedtuple
+from itertools import filterfalse
+import os
+
+
+def get_abs_real_path():
+    """Returns real absolute path to the package"""
+    try:
+        real_path = __file__ if os.path.islink(__file__) else \
+                    os.path.realpath(__file__)
+
+        return os.path.dirname(os.path.abspath(real_path))
+    except:
+        print("Something went wrong ...")
+        sys.exit()
+
+
+def sort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        point = array[0].efficiency
+        for el in array:
+            if el.efficiency < point:
+                less.append(el)
+            elif el.efficiency > point:
+                greater.append(el)
+            else:
+                equal.append(el)
+        return sort(greater) + equal + sort(less)
+    else:
+        return array
+
+
+class Knapsack(object):
+    def __init__(self, capasity=400):
+        self.max_capasity = capasity
+        self.capasity_left = capasity
+        self.stored = []
+        self.total_value = 0
+
+    def store(self, item):
+        if item.weight <= self.capasity_left:
+            self.capasity_left -= item.weight
+            self.total_value += item.value
+            return item
+        return None
+
+
+def start():
+
+    print("Package:", __package__)
+    print("File:", __file__)
+    print("Name:", __name__)
+    print("Starting application ...")
+
+    path = get_abs_real_path()
+    resource_file = os.path.join(path,'resources', 'stuff.txt')
+    with open(resource_file) as f:
+        lines = f.readlines()
+
+    Item = namedtuple("Item", 'name weight value efficiency')
+
+    def itemise(line):
+        par = line.split(',')
+        item = Item(par[0], int(par[1]), int(par[2]),
+                    int(par[2]) / int(par[1]))
+        return item
+
+    stuff = list(map(itemise, lines))
+
+    stuff = sort(stuff)
+    backpack = Knapsack()
+    backpack.stored = list(filterfalse(lambda x: x is None,
+                                       list(map(backpack.store, stuff))))
+
+    print('Space left: ' + str(backpack.capasity_left))
+    for i in backpack.stored:
+        print(i.name)
+    print('Total value: ' + str(backpack.total_value))
+
+    print("Shutting down ...")
---
+++ hw_packaged/setup.py
@@ -0,0 +1,19 @@
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__),   
+                             fname)).read()
+
+setup(
+    name='Knapsacking',
+    version='0.1',
+    description='Packaged version of code for solving knapsack problem',
+    author='Boguslav Pozniak',
+    author_email='boguslav_pozniak@epam.com',
+    url='http://www.epam.com',
+    scripts=['start_app.py',],
+    packages=['package',],
+    long_description = read('README'),
+)
